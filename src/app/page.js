af2d1843701b425f8f4c4879b9266ae80e3fdf40
1ssa10 <mahmoudisaa10@gmail.com>
2024-03-01 00:23:08 +0200

    loading

--- src/app/page.js
+++ src/app/page.js
@@ -6,39 +6,48 @@ import Navbar from "@/components/Navbar";
 import Moon from "@/components/Moon";
 import { OrbitControls, PresentationControls } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import Earth from "@/components/Earth";
 import * as THREE from "three";
 export default function Home() {
   return (
     <>
-      <Canvas
-        gl={{
-          antialias: true,
-          toneMapping: THREE.ACESFilmicToneMapping,
-          outputColorSpace: THREE.LinearSRGBColorSpace,
-        }}
-        shadows
-        camera={{
-          fov: 45,
-          near: 0.1,
-          far: 400,
-          position: [2, 0.5, 4],
-        }}
+      <Suspense
+        fallback={
+          <div className=" absolute left-1/2 top-1/2 text-white">
+            loading...
+          </div>
+        }
       >
-        <PresentationControls
-          global
-          polar={[0, 0.1]}
-          azimuth={[-0.25, 0.5]}
-          config={{ mass: 2, tension: 200 }}
+        <Canvas
+          gl={{
+            antialias: true,
+            toneMapping: THREE.ACESFilmicToneMapping,
+            outputColorSpace: THREE.LinearSRGBColorSpace,
+          }}
+          shadows
+          camera={{
+            fov: 45,
+            near: 0.1,
+            far: 400,
+            position: [2, 0.5, 4],
+          }}
         >
-          <ambientLight intensity={2} />
-          <Moon />
-          <Earth />
-        </PresentationControls>
-      </Canvas>
-      <Navbar />
-      <UI />
+          <PresentationControls
+            global
+            polar={[0, 0.1]}
+            azimuth={[-0.25, 0.5]}
+            config={{ mass: 2, tension: 200 }}
+          >
+            <ambientLight intensity={2} />
+
+            <Moon />
+            <Earth />
+          </PresentationControls>
+        </Canvas>
+        <Navbar />
+        <UI />
+      </Suspense>
     </>
   );
 }
